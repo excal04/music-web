0a61db04d6130ebd385af22dcbb2bff5c74c2c5a
excal04 <excal04@users.noreply.github.com>
2015-08-06 10:13:07 +0800

    retrieve json file

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 
 var app = angular.module("MPApp", []);
-//
-// app.run(function($rootScope) {
-//     $rootScope.name = "Jeffy";
-// });
 
-app.controller("PlayerController", ["$scope", function($scope) {
+var nprUrl = "http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON",
+    apiKey = "MDIwMDkwNDMzMDE0Mzg4MjYyMTA1YmI2Nw001";
+
+app.controller("PlayerController", ["$scope", "$http", function($scope, $http) {
     $scope.audio = document.createElement("audio");
     $scope.playing = false;
     $scope.audio.src = "./HumanBeatbox1.mp3";
@@ -26,8 +25,34 @@ app.controller("PlayerController", ["$scope", function($scope) {
             $scope.stop();
         });
     });
+
+    $http({
+        method: "JSONP",
+        url: nprUrl + "&apiKey=" + apiKey + "&callback=JSON_CALLBACK"
+    }).success(function(data, status) {
+        console.log("DATA:");
+        console.log(data);
+
+        $scope.programs = data.list.story;
+    }).error(function() {
+        // error occured
+        console.log("error");
+    });
 }]);
 
 app.controller("RelatedController", ["$scope", function($scope) {
 
 }]);
+
+app.controller("ClockController", function($scope) {
+    $scope.person = "Blue";
+    var updateClock = function() {
+        $scope.clock = new Date();
+    };
+
+    var timer = setInterval(function() {
+        $scope.$apply(updateClock());
+    }, 1000);
+
+    updateClock();
+});
